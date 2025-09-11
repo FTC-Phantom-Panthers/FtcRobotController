fe7ab57c6603880b253545ba1a4de9435c5d997f
konekowo <70107937+konekowo@users.noreply.github.com>
2025-09-11 15:54:10 -0400

    try to get camera working

--- TeamCode/src/main/java/team/phantompanthers/opcode/tele_op/impl/TeleOpNormalFunction.java
+++ TeamCode/src/main/java/team/phantompanthers/opcode/tele_op/impl/TeleOpNormalFunction.java
@@ -2,12 +2,26 @@ package team.phantompanthers.opcode.tele_op.impl;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
 import team.phantompanthers.opcode.tele_op.BaseTeleOpCode;
 
 @TeleOp(name = "Normal Function", group = "Linear Opmode")
 public class TeleOpNormalFunction extends BaseTeleOpCode {
     @Override
     public void runOpMode() {
+        initAprilTag();
 
+        waitForStart();
+        while (opModeIsActive()) {
+            telemetryAprilTag();
+
+            telemetry.update();
+
+            // im not sure why we have to yield the cpu, ftc example said to do this
+            sleep(20);
+        }
+
+        visionPortal.close();
     }
 }
